c64a4a774cbc823c5d6324b5231bb9b46e7483a1
AndreClerigo Desktop <andreclerigo@oultook.com>
2021-03-08 20:33:44 +0000

    fixing bug // in python

--- Python/Tetris/tetris.py
+++ Python/Tetris/tetris.py
@@ -128,12 +128,12 @@ T = [['.....',
  
 shapes = [S, Z, I, O, J, L, T]
 shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
- 
+
 
 class Piece(object):
     #Constructor for the shape
     def __init__(self, x, y, shape):
-        self.x = x
+        self.x = x                 #BUG shoudnt need this!
         self.y = y
         self.shape = shape
         self.color = shape_colors[shapes.index(shape)]  #Get the color by the shape_colors list
@@ -147,12 +147,12 @@ def create_grid(locked_positions={}):
             if (j, i) in locked_positions:
                 c = locked_positions[(j, i)]
                 grid[i][j] = c
-
     return grid
 
 
 def convert_shape_format(shape):
     positions = []
+    
     format = shape.shape[shape.rotation % len(shape.shape)]
 
     for i, line in enumerate(format):
@@ -163,6 +163,8 @@ def convert_shape_format(shape):
 
     for i, pos in enumerate(positions):
         positions[i] = (pos[0] - 2, pos[1] - 4)
+
+    return positions
 
 
 def valid_space(shape, grid):
@@ -188,22 +190,28 @@ def check_lost(positions):
 
 #Generate a random shape
 def get_shape():
+    global shapes, shape_colors
+
     #Generate the piece in the middle of the screen and on the top
-    return Piece(grid_columns/2 , 0, random.choice(shapes))
+    return Piece(grid_columns//2 , 0, random.choice(shapes))
 
 
 def draw_text_middle(text, size, color, surface):
-    pass
+    #Type of font
+    font = pygame.font.SysFont('arial black', 60)
+    label = font.render('Tetris', 1, (255, 255, 255))  #Label text, antialiasing, color
+
+    surface.blit(label, (top_left_x + play_height/2 - (label.get_width()/2), 30))  #Center the label on the screen
 
 
 def draw_grid(surface, grid):
     sx = top_left_x
     sy = top_left_y
 
-    for i in range(len(grid)):  #Rows
-        pygame.draw.line(surface, (128, 128, 128), (sx, sy + i*block_size), (sx + play_width, sy + i*block_size))
-        for j in range(len(grid[i])):  #Columns
-            pygame.draw.line(surface, (128, 128, 128), (sx, j*block_size, sy), (sx + j*block_size, sy + play_height))
+    for i in range(len(grid)):
+        pygame.draw.line(surface, (128,128,128), (sx, sy+ i*block_size), (sx + play_width, sy + i*block_size))  # horizontal lines
+        for j in range(len(grid[i])):
+            pygame.draw.line(surface, (128,128,128), (sx + j*block_size, sy), (sx + j*block_size, sy + play_height))  # vertical lines
     
 
 def clear_rows(grid, locked):
@@ -218,15 +226,18 @@ def draw_window(surface, grid):
     surface.fill((0, 0, 0))
 
     pygame.font.init()
-
     #Type of font
-    font = pygame.font.SysFont('arial black', 60)
+    font = pygame.font.SysFont('comicsans', 60)
     label = font.render('Tetris', 1, (255, 255, 255))  #Label text, antialiasing, color
 
-    surface.blit(label, (top_left_x + play_height/2 - (label.get_width()/2), 30))  #Center the label on the screen
+    surface.blit(label, (top_left_x + play_width/2 - (label.get_width()/2), 30))  #Center the label on the screen
+    
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            pygame.draw.rect(surface, grid[i][j], (top_left_x + j* 30, top_left_y + i * 30, 30, 30), 0)
     
     draw_grid(surface, grid)
-
+    
     pygame.display.update()
   
 
@@ -241,13 +252,23 @@ def main(win):
     next_piece = get_shape()
     clock = pygame.time.Clock()
     fall_time = 0
+    fall_speed = 0.27
 
     while run:
+        grid = create_grid(locked_positions)
+        fall_time += clock.get_rawtime() 
+        clock.tick()
+
+        if fall_time/1000 > fall_speed:
+            fall_time = 0
+            current_piece.y += 1
+            if not(valid_space(current_piece, grid) and current_piece.y > 0):
+                current_piece.y -=1
+                change_piece = True
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                pygame.display.quit()
-                quit()
 
             elif event.type == pygame.KEYDOWN:
                 #Python doesn't have switch case? rlly?
@@ -270,9 +291,30 @@ def main(win):
                     current_piece.x -= 1
                     if not(valid_space(current_piece, grid)):
                         current_piece.y -=1
+        
+        shape_pos = convert_shape_format(current_piece)
+
+        for i in range(len(shape_pos)):
+            x, y = shape_pos[i]
+            if y > -1:
+                print("y " + str(y))
+                print("x " + str(x))
+                grid[y][x] = current_piece.color
+
+        if change_piece:
+            for pos in shape_pos:
+                p = (pos[0], pos[1])
+                locked_positions[p] = current_piece.color
+            current_piece = next_piece
+            next_piece = get_shape()
+            change_piece = False
 
         draw_window(win, grid)
+        
+        if check_lost(locked_positions):
+            run = False
 
+    pygame.display.quit()
 
 def main_menu(win):
     main(win)
